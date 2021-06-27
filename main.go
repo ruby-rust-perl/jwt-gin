1aafd11d256f8d2ac43c30e15b02d8cbe50bffcd
Python.clj <49131888+python-clojure@users.noreply.github.com>
2021-06-27 15:29:35 +0530

    Added the first endpoint

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	r := gin.Default()
+
+	public := r.Group("/api")
+
+	public.POST("/register", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": "this is the register endpoint!"})
+	})
+
+	r.Run(":8080")
+
+}
